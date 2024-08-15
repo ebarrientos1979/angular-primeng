d51344b8418817a3847c4917f2455a6e9939b8e8
ebarrientos1979 <edwin.barrientos@iteraprocess.com>
2024-08-14 20:02:29 -0500

    Boton PrimeNG

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
+import { SidebarMainComponent } from './sidebar/sidebar-main/sidebar-main.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, SidebarMainComponent],
+  imports: [BrowserModule, AppRoutingModule, ButtonModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/sidebar/sidebar-main/sidebar-main.component.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-main',
+  templateUrl: './sidebar-main.component.html',
+  styleUrl: './sidebar-main.component.css',
+})
+export class SidebarMainComponent {}
