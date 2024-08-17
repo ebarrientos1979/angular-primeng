543baaa65e5cfedaa76536f33c011c180d361e5c
ebarrientos1979 <edwin.barrientos@iteraprocess.com>
2024-08-16 19:48:47 -0500

    Avance Clase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonModule } from 'primeng/button';
-import { SidebarMainComponent } from './sidebar/sidebar-main/sidebar-main.component';
+import { SidebarMainComponent } from './general/components/sidebar-main/sidebar-main.component';
 import { DataViewModule } from 'primeng/dataview';
 import { ProductoModule } from './producto/producto.module';
 import { CardModule } from 'primeng/card';
