ba99aff4bb73d1f05486d511a110c5007b1b0729
ebarrientos1979 <edwin.barrientos@iteraprocess.com>
2024-08-16 20:32:26 -0500

    Producto Venta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonModule } from 'primeng/button';
@@ -20,6 +20,7 @@ import { GeneralModule } from './general/general.module';
     CardModule,
     DataViewModule,
     GeneralModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/producto/components/card/card.component.ts
+++ src/app/producto/components/card/card.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+interface ProductoInterface {
+  oferta: number;
+  nombreProducto: string;
+  precio: number;
+  linkImagen: string;
+  descripcion: string;
+}
+
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrl: './card.component.scss'
+  styleUrl: './card.component.scss',
 })
 export class CardComponent {
-
+  producto: ProductoInterface = {
+    oferta: 1,
+    nombreProducto: 'Pechuga de Pavita',
+    precio: 13.5,
+    linkImagen:
+      'https://plazavea.vteximg.com.br/arquivos/HP-BANNERDESTACADO-FOOD-PP-D-FRESCOS-12-16-AGOSTO-JUN-5.webp',
+    descripcion: 'Pechuga de Pavita en trozos x kg',
+  };
 }
