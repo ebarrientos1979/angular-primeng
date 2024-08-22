a5b5819eb751a8c1ee963f2b097bfe93d299ab18
ebarrientos1979 <edwin.barrientos@iteraprocess.com>
2024-08-21 20:45:21 -0500

    Servicio

--- src/app/producto/components/find-producto/find-producto.component.ts
+++ src/app/producto/components/find-producto/find-producto.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductosService } from '../../services/productos.service';
+import { Producto, ProductoInterface } from '../../model/producto.model';
 
 interface AutoCompleteCompleteEvent {
   originalEvent: Event;
@@ -9,16 +11,28 @@ interface AutoCompleteCompleteEvent {
   selector: 'app-find-producto',
   templateUrl: './find-producto.component.html',
 })
-export class FindProductoComponent {
+export class FindProductoComponent implements OnInit {
   items: any[] | undefined;
-
+  productos: Producto[] = [];
   selectedItem: any;
-
   suggestions: any[] = [];
+  suggestionsOrig: any[] = [];
+
+  constructor(private productosService: ProductosService) {}
 
   search(event: AutoCompleteCompleteEvent) {
-    this.suggestions = [...Array(10).keys()].map(
-      (item) => event.query + '-' + item
-    );
+    let sugerencia = this.suggestionsOrig;
+    this.suggestions = sugerencia.filter((sug) => {
+      return sug.toLowerCase().includes(event.query.toLowerCase());
+    });
+  }
+
+  ngOnInit(): void {
+    this.productosService.getProductos().subscribe((p: ProductoInterface) => {
+      this.productos = p.productos;
+      this.productos.forEach((producto) => {
+        this.suggestionsOrig.push(producto.title);
+      });
+    });
   }
 }
